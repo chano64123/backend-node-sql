5073cbe26f1d2cd3c8afdd049e39239618b59a3d
chano64123 <promo201674@gmail.com>
2022-07-12 23:23:05 -0500

    heroku configuration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import config from './config'
 import clientsRoutes from './routes/clients.routes'
+import cors  from 'cors'
 
 const app  = express()
 
 // setings
 app.set('port', config.port)
+app.use(cors())
 
 // middlewares
 app.use(express.json())
